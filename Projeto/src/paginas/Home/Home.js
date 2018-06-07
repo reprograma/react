a4a6a5d5e4414ac6fbb00e81ab9fc8cc94a769a4
camilaibs <camilaibs@gmail.com>
2018-06-07 14:16:58 -0300

    Loading de postits

--- Projeto/src/paginas/Home/Home.js
+++ Projeto/src/paginas/Home/Home.js
@@ -1,30 +1,63 @@
 import React from 'react'
 import Postit from '../../componentes/Postit/Postit'
+import loading from './loading.gif'
 import './Home.css'
 
 
-function Home(props) {
-    return (
-        <div className="home">
-            <h1>Home</h1>
+class Home extends React.Component {
+    state = {
+        postits: []
+    }
 
-            <Postit />
+    componentDidMount() {
+        // TODO: buscar lista de postit
+        const postits = [
+            {
+                id: "c2f27308-75c6-440b-bbf9-1a075c32200f",
+                titulo: "Estuda HTML",
+                texto: "Lorem Ipsum"
+            },
+            {
+                id: "4627781c-bd04-41d5-8c97-3819d2a38fc1",
+                titulo: "Estuda CSS",
+                texto: "Lorem Ipsum"
+            },
+            {
+                id: "04b9dd92-71a6-46bc-9e63-df5112d3fa71",
+                titulo: "Estuda JS",
+                texto: "Lorem Ipsum"
+            }
+        ]
+        
+        setInterval(() => {
+            this.setState({
+                postits: postits
+            })
+        }, 3000)
+    }
 
-            <Postit
-                id="e8652ad9-e98d-49d7-a45f-6f9dc51838ad"
-                titulo="Estudar Javascript"
-                texto="Ler frontend handbook 2018"
-                editando={true}
-            />
-
-            <Postit
-                id="88de0d27-a1d7-479d-a6b7-7f2b3899b1b3"
-                titulo="Estudar HTML"
-                texto="Bla bla bla"
-                editando={false}
-            />
-        </div>
-    )
+    render() {
+        return (
+            <div className="home">
+                <h1>Home</h1>
+    
+                <Postit />
+    
+                {this.state.postits.length === 0 ? (
+                    <img src={loading} alt="Carregando lista de postit" />
+                ) : (
+                    this.state.postits.map(postit => (
+                        <Postit 
+                            key={postit.id}
+                            id={postit.id}
+                            titulo={postit.titulo}
+                            texto={postit.texto}
+                        />
+                    ))
+                )}
+            </div>
+        )
+    }
 }
 
 export default Home
